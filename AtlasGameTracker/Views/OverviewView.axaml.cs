1370465b2e97c791b88392fc0aa0eb789e2082f4
Spicy <spicy687530@gmail.com>
2026-02-07 18:24:36 -0600

    Move MainWindow to Views and add Overview/TrackedApp
    Restructure UI: move MainWindow.axaml(.cs) into Views/, add new OverviewView and TrackedAppView user controls with code-behinds, and update MainWindow to host those views in the TabControl (renamed tabs to Overview and Tracked Applications). Also add a small version watermark TextPresenter and add a ViewModel folder entry to the csproj. This organizes the UI into separate views for better modularity and future ViewModel integration.

---
+++ AtlasGameTracker/Views/OverviewView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AtlasGameTrackerUI;
+
+public partial class OverviewView : UserControl
+{
+    public OverviewView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ AtlasGameTracker/Views/TrackedAppView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AtlasGameTrackerUI;
+
+public partial class TrackedAppView : UserControl
+{
+    public TrackedAppView()
+    {
+        InitializeComponent();
+    }
+}
